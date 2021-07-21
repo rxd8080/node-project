9e6fd5fcc5cf5dfad7e3c9003ad0816acef359cb
rxdhj <578401154@qq.com>
2021-07-21 23:11:02 +0800

    连接数据库，渲染用户管理页

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
- router.get('/users.html', function(req, res) {
-   res.render('users');
- })
+// router.get('/users', function (req, res) {
+//   res.render('users');
+// })
 
- router.get('/brand.html', function(req, res) {
-   res.render('brand');
- })
+router.get('/brand', function (req, res) {
+  res.render('brand');
+})
 
- router.get('/phone.html', function(req, res) {
-   res.render('phone');
- })
+router.get('/phone', function (req, res) {
+  res.render('phone');
+})
+router.get('/login.html', function (req, res) {
+  res.render('login');
+})
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
+var url = 'mongodb://127.0.0.1:27017';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', function (req, res, next) {
+  MongoClient.connect(url, function(err, client) {
+    var db = client.db('project');
+    db.collection('user').find().toArray(function(err,data) {
+      if(err) {
+        console.log('查询用户数据失败', err);
+        res.render('error', {
+          message:'查询失败',
+          error:err
+        })
+      }else {
+        res.render('users', {
+          list:data
+        });
+      }
+    })
+  })
 });
 
+
+// router.post('/login', function(req, res) {
+//   var username = req.body.name;
+//   var password = req.body.pwd;
+//   MongoClient.connect(url, function(err, client) {
+//     var db = client.db('project');
+//     db.collection('user').find({
+//       username:username,
+//       password:password
+//     }).toArray(function(err,data){
+//       if(err) {
+//         res.render('error', {
+//           message:'查询失败',
+//           error:err
+//         })
+//       }else {
+//         res.cookie('nickname', data[0].nickname, {
+//           maxAge: 10 * 60 * 1000
+//         })
+//         res.redirect('/');
+//       }
+//       client.close();
+//     })
+//   })
+//   res.send('')
+// })
 module.exports = router;
